085a5dd7bc2dda59add7c9756685264d4c07cdb5
zack <zack.bitcoin@gmail.com>
2016-08-22 09:24:15 -0700

    update the start command so we can choose size

--- src/dump_app.erl
+++ src/dump_app.erl
@@ -1,17 +1,8 @@
 -module(dump_app).
-
 -behaviour(application).
-
-%% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(normal, [ID]) ->
-    io:fwrite("ok\n"),
-    Size = 5,
+start(normal, [ID, Size]) ->
     dump_sup:start_link(ID, Size).
 
 stop(_State) ->
