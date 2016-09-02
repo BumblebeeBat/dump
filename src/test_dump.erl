af0117d78acd55c20ea806f4d4ca6883b84f4abc
zack <zack.bitcoin@gmail.com>
2016-09-01 18:25:13 -0700

    adjust test slightly

--- src/test_dump.erl
+++ src/test_dump.erl
@@ -1,11 +1,16 @@
 -module(test_dump).
 -export([test/0]).
+%timer:tc(test_dump, test, []).
+%{5457078,success}
+
+
 test() ->
-    ID = kv,
-    dump_sup:start_link(ID, 5),
-    V1 = <<5,4,3,2,1>>,
-    V2 = <<5,4,3,2,1>>,
-    V3 = <<5,4,3,2,1>>,
+    ID = kv2,
+    Size = 100,
+    dump_sup:start_link(ID, Size),
+    V1 = <<0:(8*Size)>>,
+    V2 = <<2:(8*Size)>>,
+    V3 = <<3:(8*Size)>>,
     A1 = dump:put(V1, ID),
     V1 = dump:get(A1, ID),
     A2 = dump:put(V2, ID),
@@ -17,9 +22,9 @@ test() ->
     V3 = dump:get(A3, ID),
     dump:delete(A2, ID),
     A2 = dump:put(V1, ID),
-    Times = 10000,
-    test_times(Times, ID).
-test_times(0, _) -> success;
-test_times(N, ID) -> 
-    dump:put(<<0,0,0,0,0>>, ID),
-    test_times(N-1, ID).
+    Times = 100000,
+    test_times(Times, Size, ID).
+test_times(0, _, _) -> success;
+test_times(N, Size, ID) -> 
+    dump:put(<<0:(8*Size)>>, ID),
+    test_times(N-1, Size, ID).
