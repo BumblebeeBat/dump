fa012f022c73429562f9183873e00354e82f4a4a
Luca Favatella <luca.favatella@erlang-solutions.com>
2017-07-06 15:24:41 +0100

    AE-33 Make badmatch exception include name of file failed to open
    ... in order to ease troubleshooting.
    
    A mistake is not having created the `data` directory. See module
    `dump_sup` and script `start.sh`.

--- src/file_manager.erl
+++ src/file_manager.erl
@@ -8,7 +8,7 @@ init({Name, Size, ram}) ->
 	end,
     {ok, {Z, Name, ram}};
 init({Name, _, hd}) -> 
-    {ok, F} = file:open(Name, [write, read, raw, binary]),
+    {{ok, F}, _} = {file:open(Name, [write, read, raw, binary]), Name},
     {ok, {F, Name}}.
 start_link(File, Id, Size, Mode) -> gen_server:start_link({global, Id}, ?MODULE, {File, Size, Mode}, []).
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
