8354c76826500325a48427d7a5bfeaeae903c158
zack <zack.bitcoin@gmail.com>
2019-05-16 20:11:29 -0400

    fix typo

--- src/dump.erl
+++ src/dump.erl
@@ -28,16 +28,16 @@ terminate(_, {_, _, ID, Loc}) ->
     io:format("died!"), ok.
 handle_info(_, X) -> {noreply, X}.
 handle_cast(_, X) -> {noreply, X}.
-handle_call({delete, Location, Id}, _From, X = {hd, _}) ->
+handle_call({delete, Location, _Id}, _From, X = {hd, _, Id, _}) ->
     bits:delete(Id, Location),
     {reply, ok, X};
-handle_call({delete, Location, Id}, _From, X = {ram, _}) ->
+handle_call({delete, Location, _}, _From, X = {ram, _, Id, _}) ->
     ets:delete(Id, Location),
     {reply, ok, X};
-handle_call({update, Location, Data, ID}, _From, X = {ram, _}) ->
+handle_call({update, Location, Data, _ID}, _From, X = {ram, _, ID, _}) ->
     ets:insert(ID, [{Location, Data}]),
     {reply, ok, X};
-handle_call({update, Location, Data, ID}, _From, X = {hd, _}) ->
+handle_call({update, Location, Data, _ID}, _From, X = {hd, _, ID, _}) ->
     %{Word} = X,
     Word = size(Data),
     file_manager:write(ID, Location*Word, Data),
@@ -48,27 +48,27 @@ handle_call({update, Location, Data, ID}, _From, X = {hd, _}) ->
 %    file_manager:fast_write(ID, Top*Word, Data),
 %    bits:write(ID),
 %    {reply, Top, X};
-handle_call({write, Data, ID}, _From, {ram, Top}) ->
+handle_call({write, Data, _ID}, _From, {ram, Top, ID, Loc}) ->
     ets:insert(ID, {Top, Data}),
-    {reply, Top, {ram, Top+1}};
-handle_call({write_batch, L, ID}, _From, {ram, Top}) ->
+    {reply, Top, {ram, Top+1, ID, Loc}};
+handle_call({write_batch, L, _ID}, _From, {ram, Top, ID, Loc}) ->
     %ets:insert(ID, {Top, Data}),
     ets:insert(ID, L),
     Top2 = max_second(L, Top),
-    {reply, Top, {ram, Top2+1}};
-handle_call({write, Data, ID}, _From, X = {hd, Word}) ->
+    {reply, Top, {ram, Top2+1, ID, Loc}};
+handle_call({write, Data, _ID}, _From, X = {hd, Word, ID, Loc}) ->
     Word = size(Data),
     Top = bits:top(ID),
     file_manager:write(ID, Top*Word, Data),
     bits:write(ID),
     {reply, Top, X};
-handle_call({read, Location, ID}, _From, X = {ram, _}) ->
+handle_call({read, Location, _ID}, _From, X = {ram, _, ID, Loc}) ->
     Y = case ets:lookup(ID, Location) of
             [] -> empty;
             Z -> element(2, hd(Z))
         end,
     {reply, Y, X};
-handle_call({read, Location, ID}, _From, X = {hd, Word}) ->
+handle_call({read, Location, _ID}, _From, X = {hd, Word, ID, Loc}) ->
     Z = case file_manager:read(ID, Location*Word, Word) of
 	    {ok, A} -> A;
 	    eof -> 
@@ -85,15 +85,20 @@ handle_call({read, Location, ID}, _From, X = {hd, Word}) ->
     %            <<0:(Word*8)>>
     %    end,
     {reply, Z, X};
-handle_call(word, _From, X = {ram, _}) ->
+handle_call(word, _From, X = {ram, _, _, _}) ->
     {reply, 0, X};
-handle_call(word, _From, X = {hd, Word}) ->
+handle_call(word, _From, X = {hd, Word, _, _}) ->
     {reply, Word, X};
-handle_call({highest, _ID}, _From, X = {ram, Top}) ->
+handle_call({highest, _ID}, _From, X = {ram, Top, _, _}) ->
     {reply, Top, X};
-handle_call({highest, ID}, _From, X = {hd, Word}) ->
+handle_call({highest, ID}, _From, X = {hd, Word, _, _}) ->
     A = bits:highest(ID),
-    {reply, A*Word, X}.
+    {reply, A*Word, X};
+handle_call(Other, _, X) ->
+    io:fwrite("dump cannot handle that command\n"),
+    io:fwrite("\n"),
+    io:fwrite({Other, X}),
+    {reply, ok, X}.
 
 
 max_second([], X) -> X;
